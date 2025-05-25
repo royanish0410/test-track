b049e46fb5bae085201e7636064aab3794b362dc
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-05-26 02:33:28 +0530

    Prisma instance created.

---
+++ src/lib/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from '@prisma/client'
+import { PrismaNeon } from '@prisma/adapter-neon'
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
