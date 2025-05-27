0aaa1afabcccf49594bfe76d165dfb82968c965d
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-05-27 18:12:18 +0530

    Prisma Schema updated and utility function updated.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,19 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@/generated/prisma'
 import { PrismaNeon } from '@prisma/adapter-neon'
 
-const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
-const prisma = new PrismaClient({ adapter })
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
 
-export { prisma }
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({
+    adapter:adapter,
+    log: process.env.NODE_ENV === 'development' ? ['query', 'error', 'warn'] : ['error']
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prisma
+}
+
+export default prisma
