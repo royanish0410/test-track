5e72cc5f10026e7070a691164f0222cc108dcfcd
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-05-29 17:36:35 +0530

    Student and teacher dashboard page only created.

---
+++ src/app/teacher/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='min-h-screen w-full flex justify-center items-center bg-neutral-950 text-orange-50 text-6xl'>
+        Teacher Dashboard
+        </div>
+    )
+}
+
+export default page
