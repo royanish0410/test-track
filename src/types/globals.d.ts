3c63c7829298b715746a3cdddda3db7ab12781e5
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-05-29 14:25:46 +0530

    Types are updated for the Role of the User.

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -3,7 +3,7 @@ export {}
 declare global {
     interface CustomJwtSessionClaims {
         metadata: {
-        role?: 'admin' | 'moderator' | 'contributor' | 'viewer'
+        role?: "STUDENT" | "TEACHER"
         }
     }
 }
