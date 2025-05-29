c714566c5fc609f8eb00ff265606d59a7dfc5c3a
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-05-30 00:36:29 +0530

    Student and Teacher Dashboard SignOut component added, customization can be done later.

--- src/app/student/dashboard/page.tsx
+++ src/app/student/dashboard/page.tsx
@@ -1,9 +1,15 @@
+import { SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 const page = () => {
     return (
-        <div className='min-h-screen w-full flex justify-center items-center bg-neutral-950 text-orange-50 text-6xl'>
+        <div className='min-h-screen w-full flex flex-col justify-center items-center bg-neutral-950 text-orange-50 text-6xl'>
         Student Dashboard
+        <div>
+            <SignOutButton>
+                SIGNOUT
+            </SignOutButton>
+        </div>
         </div>
     )
 }
