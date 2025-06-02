82c7bd81e5ab0c6107890a69c1e29d63162376d6
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-06-03 00:57:41 +0530

    Global ts interfaces updated with the new files.

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -1,9 +1,31 @@
 export {}
 
+export type SortType = "ATIME" | "DTIME" | "ADUR" | "DDUR" | "AENDS" | "DENDS";
+
 declare global {
     interface CustomJwtSessionClaims {
         metadata: {
         role?: "STUDENT" | "TEACHER"
         }
+    }
+
+    /**
+    * Sorting types for various operations.
+    * 
+    * - "ATIME": Ascending by time.
+    * - "DTIME": Descending by time.
+    * - "ADUR": Ascending by duration.
+    * - "DDUR": Descending by duration.
+    * - "AENDS": Ascending by end timestamp.
+    * - "DENDS": Descending by end timestamp.
+    */
+
+    interface sorting{
+        /**
+        * Sorting option to apply.
+        * 
+        * Example: "ATIME" for ascending by time.
+        */
+        sortType: SortType
     }
 }
