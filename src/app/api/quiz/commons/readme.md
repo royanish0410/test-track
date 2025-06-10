6a56489f45f0a6da44ed2c6a7348b8dbbb486734
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-06-10 14:48:09 +0530

    Get signature-url for the secure upload route created. and readme of it.

--- src/app/api/quiz/commons/readme.md
+++ src/app/api/quiz/commons/readme.md
@@ -31,3 +31,40 @@
 ```
 
 ***
+
+# Get Signed Url for secure uploading
+## Method: GET
+## Route: /api/quiz/commons/signature-url
+
+## Request Body >
+```
+{}
+```
+## Response Body >
+```
+{
+    "signature": "120cfcfeb9c9de30adbed13106188da1ad94ba79",
+    "timestamp": 1749546412,
+    "folder": "MockQuiz",
+    "max_file_size": 7000000,
+    "resource_type": "image",
+    "expires_at": 1749547012,
+    "api_key": "867554791284663",
+    "cloud_name": "dy26w6o2h"
+}
+```
+Create a formBody with the following key and values
+
+| Key           | Value          | Description             |
+| ------------- | -------------- | ----------------------- |
+|  file         | file           | the file you want to upload |
+|  signature    | string         | Signature for the secure authentic upload |
+|  timestamp    | number         | the timestamp at which the signed url created |
+|  folder       | string         | folder name where the files would be stored(mention the one that is obtained from the response)            |
+| max_file_size | number         | this is the maximum size of the file to upload            |
+| resource_type | string         | file type            |
+|  expires_at   | string         | the timestamp when the url get invalid           |
+|  api_key      | string         | Api key used for the authentication of signature             |
+|  cloud_name   | string         | the cloudname where you would upload the files            |
+
+***
--- src/app/api/quiz/commons/signature-url/route.ts
+++ src/app/api/quiz/commons/signature-url/route.ts
@@ -2,14 +2,36 @@ import { v2 as cloudinary } from "cloudinary";
 import { NextResponse, NextRequest } from 'next/server'
 
 cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+    
 })
 
-export async function GET(request: NextRequest) {
+export async function GET(_: NextRequest) {
     try {
+        const timeStamp = Math.round(new Date().getTime() / 1000);
+        const signature = cloudinary.utils.api_sign_request({
+            timestamp: timeStamp,
+            folder: 'MockQuiz',
+            max_file_size: 7000000,
+            resource_type: 'image',
+            expires_at: timeStamp + 600
+        }, process.env.CLOUDINARY_API_SECRET);
 
+        return NextResponse.json({
+            signature:signature,
+            timestamp:timeStamp,
+            folder:'MockQuiz',
+            max_file_size:7000000,
+            resource_type: 'image',
+            expires_at: timeStamp + 600,
+            api_key:process.env.CLOUDINARY_API_KEY,
+            cloud_name:process.env.CLOUDINARY_CLOUD_NAME
+        })
     } catch (error) {
-        console.log('Error at --------- +++++++++ ::::: ');
+        console.log('Error at generating signed-url ::::: ');
         console.error(error);
-        return NextResponse.json({message:'Something Went Wrong during --------- +++++++++.',error:error},{status:500});
+        return NextResponse.json({message:'Something Went Wrong during generating signed-url.',error:error},{status:500});
     }
 }
